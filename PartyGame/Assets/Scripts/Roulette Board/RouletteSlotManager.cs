fbd33bdd2054c643ded1fc461633d4c02d719fbf
mitchellthomson <mitchell.thomson@code.berlin>
2024-02-01 20:02:55 -0800

    Added wheel test scene

---
+++ PartyGame/Assets/Scripts/Roulette Board/RouletteSlotManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteSlotManager : MonoBehaviour
+{
+    public GameObject lane;
+    
+    void Start()
+    {
+        
+    }
+
+}
